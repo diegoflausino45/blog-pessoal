b46ba7056f7a601dda43afb9f22447371655f153
Diego Flausino <diegocflausino@gmail.com>
2025-12-17 23:26:48 -0300

    Commit create component header

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./Components/Header/Header";
+
 import Home from "./Pages/Home/home";
 
 
 function RouterApp(){
     return(
         <BrowserRouter>
+        <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
             </Routes>
